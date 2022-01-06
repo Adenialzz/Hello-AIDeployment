58fc214a7ee5d57fb48ccd093ea55dec8e732c17
Adenialzz <1160180309@qq.com>
2022-01-06 20:42:18 +0800

    fix bugs

--- HAID/opencv_dnn/get_model_file.sh
+++ HAID/opencv_dnn/get_model_file.sh
@@ -1,3 +1,3 @@
-mkdir model_file && cd model_file
-wget https://pjreddie.com/media/files/yolov3-tiny.weights
+mkdir model_file
+wget https://pjreddie.com/media/files/yolov3-tiny.weights -O model_file/yolov3-tiny.weights
 wget https://github.com/pjreddie/darknet/blob/master/cfg/yolov3-tiny.cfg?raw=true -O model_file/yolov3-tiny.cfg
